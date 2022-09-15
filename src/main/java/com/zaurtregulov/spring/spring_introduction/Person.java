d4970fff297a2a76c03e23f2337ea43fdab22171
VladimirSafronov <safron5000@mail.ru>
2022-09-15 09:28:08 +0300

    annotation @Qualifier

--- src/main/java/com/zaurtregulov/spring/spring_introduction/Person.java
+++ src/main/java/com/zaurtregulov/spring/spring_introduction/Person.java
@@ -1,25 +1,30 @@
 package com.zaurtregulov.spring.spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
+
+//    @Autowired
+//    @Qualifier("dog")
     private Pet pet;
     private String surname;
     private int age;
 
     @Autowired
-    public Person(Pet pet) {
+    public Person(@Qualifier("dog") Pet pet) {
         System.out.println("Person bean is created");
         this.pet = pet;
     }
 
+    public Person() {
+        System.out.println("Person bean is created");
+    }
 
-//    public Person() {
-//        System.out.println("Person bean is created");
-//    }
-
+//    @Autowired
+//    @Qualifier("catBean")
     public void setPet(Pet pet) {
         this.pet = pet;
     }
