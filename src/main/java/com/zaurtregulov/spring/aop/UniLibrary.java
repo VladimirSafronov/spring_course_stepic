e935c01737f90d4712db46e505e190b0ac8a7ec2
VladimirSafronov <safron5000@mail.ru>
2022-09-15 21:03:05 +0300

    Pointcut (chapter 2)

--- src/main/java/com/zaurtregulov/spring/aop/UniLibrary.java
+++ src/main/java/com/zaurtregulov/spring/aop/UniLibrary.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class UniLibrary extends AbstractLibrary {
 
-    @Override
-    public void getBook() {
-        System.out.println("Мы получили книгу из UniLibrary");
+//    @Override
+    public void getBook(Book book) {
+        System.out.println("Мы получили книгу из UniLibrary " + book.getName());
     }
 
     public void returnBook() {
