24faa15fdeafbfbba4c8573ecd4dbe677003b875
VladimirSafronov <safron5000@mail.ru>
2022-09-16 18:53:00 +0300

    execution order Aspects

---
+++ src/main/java/com/zaurtregulov/spring/aop/aspects/LoggingAspect.java
@@ -0,0 +1,57 @@
+package com.zaurtregulov.spring.aop.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Order(10)
+public class LoggingAspect {
+
+//    @Pointcut("execution(* com.zaurtregulov.spring.aop.UniLibrary.*(..))")
+//    private void allMethodsFromUniLibrary() {}
+//
+//    @Pointcut("execution(public void com.zaurtregulov.spring.aop.UniLibrary.returnMagazine())")
+//    private void returnMagazineFromUniLibrary() {}
+//
+//    @Pointcut("allMethodsFromUniLibrary() && !returnMagazineFromUniLibrary()")
+//    private void allMethodsExceptReturnMagazineFromUniLibrary() {}
+//
+//    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+//    public void beforeAllMethodsExceptReturnMagazineFromUniLibrary() {
+//        System.out.println("beforeAllMethodsExceptReturnMagazineFromUniLibrary: writing Log #10");
+//    }
+
+
+//    @Pointcut("execution(* com.zaurtregulov.spring.aop.UniLibrary.get*())")
+//    private void allGetMethodsFromUniLibrary() {}
+//
+//    @Pointcut("execution(* com.zaurtregulov.spring.aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromUniLibrary() {}
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary() {}
+//
+//    @Before("allGetMethodsFromUniLibrary()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice: writing Log #1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice() {
+//        System.out.println("beforeReturnLoggingAdvice: writing Log #2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void beforeGetAndReturnMethodsFromUniLibrary() {
+//        System.out.println("beforeGetAndReturnMethodsFromUniLibrary: writing Log #3");
+//    }
+
+
+    @Before("com.zaurtregulov.spring.aop.aspects.MyPointcuts.allGetMethods()")
+    public void beforeGetLoginAdvice() {
+        System.out.println("beforeGetBookAdvice: логирование попытки получить издание");
+    }
+}
