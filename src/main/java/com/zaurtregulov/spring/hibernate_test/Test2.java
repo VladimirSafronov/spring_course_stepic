dc0d44ef6d443cd04c45587fd347c757893d5e26
VladimirSafronov <safron5000@mail.ru>
2022-09-23 10:35:50 +0300

    got object from DB

--- src/main/java/com/zaurtregulov/spring/hibernate_test/Test2.java
+++ src/main/java/com/zaurtregulov/spring/hibernate_test/Test2.java
@@ -5,8 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
-
+public class Test2 {
   public static void main(String[] args) {
 
     try (SessionFactory factory = new Configuration()
@@ -15,11 +14,18 @@ public class Test1 {
         .buildSessionFactory()) {
 
       Session session = factory.getCurrentSession(); // открытие транзакции
-      Employee emp = new Employee("Vasiliy", "Terkin", "sales", 400);
+      Employee emp = new Employee("Elena", "Bukina", "dussh", 100);
       session.beginTransaction();
       session.save(emp);
       session.getTransaction().commit(); // закрытие транзакции
 
+      int myId = emp.getId();
+      session = factory.getCurrentSession();
+      session.beginTransaction();
+      Employee employee = session.get(Employee.class, myId); // получение объекта из базы данных
+      session.getTransaction().commit();
+
+      System.out.println(employee);
       System.out.println("Done!");
     }
   }
--- src/main/java/com/zaurtregulov/spring/hibernate_test/Test3.java
+++ src/main/java/com/zaurtregulov/spring/hibernate_test/Test3.java
@@ -1,11 +1,12 @@
 package com.zaurtregulov.spring.hibernate_test;
 
 import com.zaurtregulov.spring.hibernate_test.entity.Employee;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test3 {
 
   public static void main(String[] args) {
 
@@ -14,12 +15,16 @@ public class Test1 {
         .addAnnotatedClass(Employee.class)
         .buildSessionFactory()) {
 
-      Session session = factory.getCurrentSession(); // открытие транзакции
-      Employee emp = new Employee("Vasiliy", "Terkin", "sales", 400);
+      Session session = factory.getCurrentSession();
       session.beginTransaction();
-      session.save(emp);
-      session.getTransaction().commit(); // закрытие транзакции
 
+      List<Employee> emps = session.createQuery("FROM Employee WHERE department = 'sales'" +
+          "AND salary > 500").getResultList();
+      for(Employee e : emps) {
+        System.out.println(e);
+      }
+
+      session.getTransaction().commit();
       System.out.println("Done!");
     }
   }
