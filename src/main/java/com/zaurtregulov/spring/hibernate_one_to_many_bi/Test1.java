de8796b8ebb78446413bf05cd5be8a1c400adc75
VladimirSafronov <safron5000@mail.ru>
2022-09-24 15:13:50 +0300

    used relationship one-to-many (bi-directional)

---
+++ src/main/java/com/zaurtregulov/spring/hibernate_one_to_many_bi/Test1.java
@@ -0,0 +1,54 @@
+package com.zaurtregulov.spring.hibernate_one_to_many_bi;
+
+import com.zaurtregulov.spring.hibernate_one_to_many_bi.entity.Department;
+import com.zaurtregulov.spring.hibernate_one_to_many_bi.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+
+  public static void main(String[] args) {
+
+    SessionFactory factory = new Configuration()
+        .configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Employee.class)
+        .addAnnotatedClass(Department.class)
+        .buildSessionFactory();
+
+    Session session = null;
+
+    try {
+      session = factory.getCurrentSession();
+      session.beginTransaction();
+
+      // запись значений в БД
+//      Department dep = new Department("IT", 300, 1200);
+//      Employee emp1 = new Employee("Zaur", "Tregulov", 800);
+//      Employee emp2 = new Employee("Elena", "Popova", 1000);
+//      dep.addEmployeeToDepartment(emp1);
+//      dep.addEmployeeToDepartment(emp2);
+//      session.save(dep);
+
+      // получение значений из БД
+//      Department department = session.get(Department.class, 1);
+//      System.out.println(department);
+//      System.out.println(department.getEmps());
+//      Employee employee = session.get(Employee.class, 1);
+//      System.out.println(employee);
+//      System.out.println(employee.getDepartment());
+
+      // удаление значений из БД (Учитывай каскадные данные!)
+//      Employee employee = session.get(Employee.class, 2);
+//      session.delete(employee);
+
+      session.getTransaction().commit();
+      System.out.println("Done!");
+    }
+    finally {
+      session.close();
+      factory.close();
+    }
+  }
+
+}
--- src/main/java/com/zaurtregulov/spring/hibernate_one_to_one/Test1.java
+++ src/main/java/com/zaurtregulov/spring/hibernate_one_to_one/Test1.java
@@ -1,8 +1,8 @@
-package com.zaurtregulov.spring.hibernate_test_2;
+package com.zaurtregulov.spring.hibernate_one_to_one;
 
 
-import com.zaurtregulov.spring.hibernate_test_2.entity.Detail;
-import com.zaurtregulov.spring.hibernate_test_2.entity.Employee;
+import com.zaurtregulov.spring.hibernate_one_to_one.entity.Detail;
+import com.zaurtregulov.spring.hibernate_one_to_one.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
