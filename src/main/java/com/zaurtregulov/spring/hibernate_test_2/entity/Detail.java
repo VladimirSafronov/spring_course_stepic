bebb1d1997c0308a2112a5bdc9abc30ecf8974b3
VladimirSafronov <safron5000@mail.ru>
2022-09-24 11:32:20 +0300

    used relationship one-to-one (bi-directional)

--- src/main/java/com/zaurtregulov/spring/hibernate_test_2/entity/Detail.java
+++ src/main/java/com/zaurtregulov/spring/hibernate_test_2/entity/Detail.java
@@ -1,10 +1,12 @@
 package com.zaurtregulov.spring.hibernate_test_2.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -24,6 +26,10 @@ public class Detail {
 
   @Column(name = "email")
   private String email;
+
+  @OneToOne(mappedBy = "empDetail",
+      cascade = {CascadeType.REFRESH, CascadeType.PERSIST})
+  private Employee employee;
 
   public Detail() {
   }
@@ -64,6 +70,14 @@ public class Detail {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public Employee getEmployee() {
+    return employee;
+  }
+
+  public void setEmployee(Employee employee) {
+    this.employee = employee;
   }
 
   @Override
